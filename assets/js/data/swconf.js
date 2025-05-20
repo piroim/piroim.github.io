e1e96f829da214fbf0cbe98773108feb2fe6b10e
piro <vlfgh1456@gmail.com>
2025-05-20 22:20:36 +0900

    test

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742626827',resources: [
+    cacheName: 'chirpy-1746939127',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
